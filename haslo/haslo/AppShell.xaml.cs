a23adbdd4a3f4e97bd362731f45a606892e5f9cd
k-scibor <karolina.scibor1@wp.pl>
2025-05-24 16:38:40 +0200

    v2

---
+++ haslo/haslo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace haslo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ haslo/haslo/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿namespace haslo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        string haslo = "";
+        async void zatwierdz(object sender, EventArgs e)
+        {
+            await DisplayAlert("Dane pracownika:",$"{imie.Text}, {nazwisko.Text}, {stanowisko.SelectedItem}, hasło:{haslo}","OK");
+        }
+        private void generujHaslo(object sender, EventArgs e)
+        {
+            haslo = "";
+            var litery = "abcdefghijklmnopqrstuvwxyz";
+            var cyfry = "0123456789";
+            var wielkieLitery = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var specjalne = "!@#$%^&*()_+-=";
+            var n = Int32.Parse(ileZnakow.Text);
+            Random random = new Random();
+            if (wielkieCb.IsChecked == true)
+            {
+                int w = random.Next(wielkieLitery.Length);
+                haslo += wielkieLitery[w];
+                n--;
+            }
+            if (cyfryCb.IsChecked == true)
+            {
+                int c = random.Next(cyfry.Length);
+                haslo += cyfry[c];
+                n--;
+            }
+            if (specjalneCb.IsChecked == true)
+            {
+                int s = random.Next(specjalne.Length);
+                haslo += specjalne[s];
+                n--;
+            }
+            for (int i = 0; i < n; i++) {
+                
+                int l = random.Next(litery.Length);
+                haslo += litery[l];
+                
+            }
+        }
+    }
+
+}
